ceb7e9d0ad82e74136cc586b0b74296d424e03ca
Adam Dunmore <adamfdunmore@gmail.com>
2025-11-11 15:05:45 +0000

    Activation script test

--- module.nix
+++ module.nix
@@ -4,7 +4,10 @@ let
     inherit (lib) mkIf;
 in
 {
-    config = mkIf cfg.enable{
-        home.file.${cfg.path}.text = cfg.config;
+    config = mkIf cfg.enable {
+        home.activation.tf2nix = lib.hm.dag.entryAfter ["writeBoundary"] ''
+            mkdir ${cfg.path}
+            cat > "${cfg.path}/autoexec.cfg" << 'fov_desired 90.00'
+        '';
     };
 }
